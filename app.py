751126744fdde68cc8f98aab2a31ba064000856a
Yarfa Anayet <syedyarfa@gmail.com>
2026-05-14 23:08:50 +0530

    Again width fixed

--- app.py
+++ app.py
@@ -247,7 +247,6 @@ def build_semester_subject_graph(subjects):
     xaxis_title="",
     yaxis_title="",
     height=max(300, len(subjects) * 120),
-    width=900,
 
     xaxis=dict(showticklabels=False, showgrid=False, zeroline=False),
     yaxis=dict(autorange="reversed"),
@@ -304,7 +303,6 @@ def build_syllabus_graph(units):
         xaxis=dict(showticklabels=False, showgrid=False, zeroline=False),
 
         height=max(300, len(units) * 120),
-        width=900,
 
         margin=dict(l=100, r=50, t=80, b=30),
 
